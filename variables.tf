695a7a4e0fc7d56cb3a3ae59a6b3e1e263c84bb0
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-20 14:50:41 +0200

    fix: validation of variables

--- variables.tf
+++ variables.tf
@@ -54,11 +54,19 @@ variable "resource_group" {
 variable "os_type" {
   description = "(Required) The O/S type for the App Services to be hosted in this plan. Possible values include `Windows`, `Linux`, and `WindowsContainer`."
   type        = string
+  validation {
+    condition     = can(regex("^(Windows|Linux|WindowsContainer)$", var.os_type))
+    error_message = "Possible values include `Windows`, `Linux`, and `WindowsContainer`."
+  }
 }
 
 variable "sku_name" {
   description = "(Required) The SKU for the plan. Possible values include `B1`, `B2`, `B3`, `D1`, `F1`, `I1`, `I2`, `I3`, `I1v2`, `I2v2`, `I3v2`, `P1v2`, `P2v2`, `P3v2`, `P1v3`, `P2v3`, `P3v3`, `S1`, `S2`, `S3`, `SHARED`, `EP1`, `EP2`, `EP3`, `WS1`, `WS2`, `WS3`, and `Y1`."
   type        = string
+  validation {
+    condition     = can(regex("^(B1|B2|B3|D1|F1|I1|I2|I3|I1v2|I2v2|I3v2|P1v2|P2v2|P3v2|P1v3|P2v3|P3v3|S1|S2|S3|SHARED|EP1|EP2|EP3|WS1|WS2|WS3|Y1)$", var.sku_name))
+    error_message = "Possible values include `B1`, `B2`, `B3`, `D1`, `F1`, `I1`, `I2`, `I3`, `I1v2`, `I2v2`, `I3v2`, `P1v2`, `P2v2`, `P3v2`, `P1v3`, `P2v3`, `P3v3`, `S1`, `S2`, `S3`, `SHARED`, `EP1`, `EP2`, `EP3`, `WS1`, `WS2`, `WS3`, and `Y1`."
+  }
 }
 
 variable "app_service_environment_id" {
